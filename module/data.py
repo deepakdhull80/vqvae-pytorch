dc4d96bb66de80e856a54cf56c98d03fae79f15c
deepak.dhull <deepak.dhull@verse.in>
2024-07-19 00:34:32 +0530

    coco dataset added with transformations

--- module/data.py
+++ module/data.py
@@ -1,6 +1,18 @@
-from typing import Dict, Tuple
+import os
+import random
+from typing import Dict, Tuple, List
 import torch
 from torch.utils.data import Dataset, DataLoader, default_collate
+import torchvision
+from torchvision.transforms import (
+    Compose,
+    RandomAdjustSharpness,
+    RandomCrop,
+    RandomHorizontalFlip,
+    RandomVerticalFlip,
+    Resize,
+    RandomAutocontrast,
+)
 
 
 class RandomDataset(Dataset):
@@ -14,19 +26,71 @@ class RandomDataset(Dataset):
         return self.n_samples
 
 
+class Transformer(object):
+    def __init__(self, cfg: Dict) -> None:
+        self.augments = Compose(
+            [
+                Resize(size=(cfg["img_shape"], cfg["img_shape"]), antialias=True),
+                RandomHorizontalFlip(p=cfg["data"]["augmentation_probability"]),
+                RandomVerticalFlip(p=cfg["data"]["augmentation_probability"]),
+                RandomAdjustSharpness(2, p=cfg["data"]["augmentation_probability"]),
+                RandomAutocontrast(p=cfg["data"]["augmentation_probability"]),
+            ]
+        )
+
+    def __call__(self, x: torch.Tensor):
+        return self.augments(x)
+
+
+class COCODataset(Dataset):
+    def __init__(self, cfg: Dict, image_list: List) -> None:
+        super().__init__()
+        self.cfg = cfg
+        self.data_prefix_path = cfg["data"]["data_prefix_path"]
+        self.image_list = image_list
+        self.transform = Transformer(cfg)
+
+    def normalize_img(self, img: torch.Tensor) -> torch.Tensor:
+        return (img / 255.0 - 0.5) / 0.5
+
+    def __getitem__(self, index):
+        file = self.image_list[index]
+        path = f"{self.data_prefix_path}/{file}"
+        img = torchvision.io.read_image(path)
+        img = self.transform(img)
+        img = img.float()
+        img = self.normalize_img(img)
+        return img
+
+    def __len__(self):
+        return len(self.image_list)
+
+
 def custom_collate_fn(batch: torch.Tensor):
     return default_collate(batch)
 
 
 def get_dataloader(cfg: Dict) -> Tuple[DataLoader, DataLoader]:
+    random.seed(cfg["random_seed"])
 
-    train_ds = RandomDataset(1000)
-    val_ds = RandomDataset(100)
+    # These comments are for debugging purposes
+    # train_ds = RandomDataset(1000)
+    # val_ds = RandomDataset(100)
+
+    imgs = os.listdir(cfg["data"]["data_prefix_path"])
+    random.shuffle(imgs)
+
+    train_size = int(len(imgs) * cfg["data"]["train_size"])
+    train_image_li = imgs[:train_size]
+    val_image_li = imgs[train_size:]
+
+    train_ds = COCODataset(cfg=cfg, image_list=train_image_li)
+    val_ds = COCODataset(cfg=cfg, image_list=val_image_li)
 
     train_dl = DataLoader(
         train_ds,
         batch_size=cfg["data"]["batch_size"],
-        collate_fn=custom_collate_fn,
+        # collate_fn=custom_collate_fn,
         shuffle=True,
         num_workers=cfg["data"]["num_workers"],
         pin_memory=True,
@@ -34,7 +98,7 @@ def get_dataloader(cfg: Dict) -> Tuple[DataLoader, DataLoader]:
     val_dl = DataLoader(
         val_ds,
         batch_size=cfg["data"]["batch_size"],
-        collate_fn=custom_collate_fn,
+        # collate_fn=custom_collate_fn,
         num_workers=cfg["data"]["num_workers"],
         pin_memory=True,
     )
